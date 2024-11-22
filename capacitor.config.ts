b05a62df9ca388e62f071d63c0e9d5e5e8fa17ea
AleXCS003 <alexd003cardenas@gmail.com>
2024-11-22 14:09:16 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'examenApps',
+  webDir: 'www'
+};
+
+export default config;
