f5499bd4d687a564881364213ad342a005801a30
lenintoto <lenincgg_aries1@hotmail.com>
2024-11-22 23:40:13 -0500

    Examen 1 bimestre

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'examenApps',
-  webDir: 'dist/my-app',
+  webDir: 'www',
   bundledWebRuntime: false,
   plugins: {
     SplashScreen: {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { SplashScreen } from '@capacitor/splash-screen';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   constructor() {
